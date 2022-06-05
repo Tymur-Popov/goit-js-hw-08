d595fbd5018c7029ced6bdc3a2538d7fd3be24bb
Tymur Popov <bs2timur@gmail.com>
2022-06-05 22:07:49 +0300

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,26 +3,26 @@ import throttle from 'lodash.throttle';
 const feedbackForm = document.querySelector('.feedback-form');
 const localStorageKey = 'feedback-form-state';
 
-initForm();
+initialForm()
 
-feedbackForm.addEventListener('input', throttle(onInput, 500));
-
+feedbackForm.addEventListener('input', throttle(onInput,500));
+    
 function onInput() {
-  const formData = new FormData(feedbackForm);
-  let userForm = {};
-  formData.forEach((value, name) => (userForm[name] = value));
-  localStorage.setItem(localStorageKey, JSON.stringify(userForm));
-}
-
-function initForm() {
-  let persistedForm = localStorage.getItem('localStorageKey');
-  if (persistedForm) {
-    persistedForm = JSON.parse(persistedForm);
-    console.log(persistedForm);
-    Object.entries(persistedForm).forEach(([name, value]) => {
-      form.elements[name].value = value;
-    });
-  }
+    const formData = new FormData(feedbackForm);
+    let userForm = {};
+    formData.forEach((value, name) => (userForm[name] = value));
+    localStorage.setItem(localStorageKey, JSON.stringify(userForm));
+};
+    
+function initialForm() {
+    let persistedForm = localStorage.getItem('localStorageKey');
+    if (persistedForm) {
+        persistedForm = JSON.parse(persistedForm);
+        console.log(persistedForm);
+        Object.entries(persistedForm).forEach(([name, value]) => {
+        form.elements[name].value = value;
+        });
+    }
 }
 
 feedbackForm.addEventListener('submit', onSubmit);
